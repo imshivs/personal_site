9150b474c92aaee18d99add4b480a78a9f74b4e4
Shivani Negi <negishivs@gmail.com>
2015-06-02 22:09:32 -0700

    try swank

---
+++ server.js
@@ -0,0 +1,18 @@
+// var express = require("express");
+// var app     = express();
+// var path    = require("path");
+
+
+// app.get('/',function(req,res){
+//   res.sendFile(path.join(__dirname+'/index.html'));
+//   //__dirname : It will resolve to your project folder.
+// });
+
+// app.use(express.static(path.join(__dirname + 'css')));
+// app.use(express.static(path.join(__dirname + 'img')));
+// app.use(express.static(path.join(__dirname + 'js')));
+
+
+// app.listen(5000);
+
+// console.log("Running at Port 5000");
